6371ba16436da1a33b43db2449f79ece506dd425
Ibrahem Ashour <100946510+IbrahemAshour@users.noreply.github.com>
2023-10-28 09:50:39 +0200

    LED & Switch & 7 Segment

---
+++ AVR_Task/Week_3/SS_LT/Debug/S_Segment_prog.d
@@ -0,0 +1,12 @@
+S_Segment_prog.o S_Segment_prog.o: ../S_Segment_prog.c ../stdTypes.h \
+  ../Error_State.h ../DIO_int.h ../S_Segment_config.h ../S_Segment_priv.h
+
+../stdTypes.h:
+
+../Error_State.h:
+
+../DIO_int.h:
+
+../S_Segment_config.h:
+
+../S_Segment_priv.h:
