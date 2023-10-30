7c2dd9af57e54d8d7f5785b422a03f861c1114d3
Ibrahem Ashour <100946510+IbrahemAshour@users.noreply.github.com>
2023-10-30 11:10:33 +0200

    7 Segment precompiled

---
+++ AVR_Task/Week_3/7_Segment/Debug/DIO_prorg.d
@@ -0,0 +1,12 @@
+DIO_prorg.o DIO_prorg.o: ../DIO_prorg.c ../stdTypes.h ../Error_State.h \
+  ../DIO_int.h ../DIO_private.h ../DIO_config.h
+
+../stdTypes.h:
+
+../Error_State.h:
+
+../DIO_int.h:
+
+../DIO_private.h:
+
+../DIO_config.h:
