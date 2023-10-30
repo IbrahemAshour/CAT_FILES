36f397ae21f8f1a629cea4d08334b8bbec147fe2
Ibrahem Ashour <100946510+IbrahemAshour@users.noreply.github.com>
2023-10-30 11:07:58 +0200

    Switch

---
+++ AVR_Task/Week_3/Switch/Debug/main.d
@@ -0,0 +1,12 @@
+main.o main.o: ../main.c ../stdTypes.h ../Error_State.h ../DIO_int.h \
+  ../Switch_config.h ../Switch_int.h
+
+../stdTypes.h:
+
+../Error_State.h:
+
+../DIO_int.h:
+
+../Switch_config.h:
+
+../Switch_int.h:
