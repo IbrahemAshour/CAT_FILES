a6a425e7e8877f96764940a185f01031111cbc63
Ibrahem Ashour <100946510+IbrahemAshour@users.noreply.github.com>
2023-10-30 17:30:53 +0200

    git --help exit ls ld cd
    exit
    
    "DIO_DRIVER"
    
    exit
    
    Changes not staged for commit:
    DIO_Driver
    .
    git commit -m "DIO_Driver" week_2
    .
    .
    sadgdamls
    agsklhgw
     wg
     sdads;lga
     sj
     ma
     fa;lmvnalgnds;
     anv;l
    
     	'
    		;'	KM
    		M ;LN
    		S
    		SAS6A
    		+59+-*
    		*
    		/5
    
    		SAD'L';
    		1
    		/
    
    		/
    		/
    		/
    		/
    		\
    		\
    	git--help
    
    :q!

--- AVR_Task/week_2/Task_4/DIO_Driver/main.c
+++ AVR_Task/week_2/Task_4/DIO_Driver/main.c
@@ -4,10 +4,27 @@
  *  Created on: Oct 3, 2023
  *      Author: Alaraby
  */
-#include "DIO_int.h"
-int main()
-{
 
+#include <util/delay.h>
+#include "stdTypes.h"
+#include "Error_State.h"
+#include "DIO_int.h"
+int main(void)
+{
+	u8 Segment[]={0x3F,0x06,0x5B,
+				  0x4F,0x66,0x6D,0x7D,
+				  0x07,0x7F,0x6F
+					 },segment_counter=0;
+	DIO_enuInit();
+	DIO_enuInit();
+	while(1)
+	{
+		if(segment_counter<=8){
+		DIO_enuSet_PortValue(DIO_u8PORTC,Segment[segment_counter]);
+		_delay_ms(500);
+		segment_counter++;
+		}
+	}
 	return 0;
 }
 
