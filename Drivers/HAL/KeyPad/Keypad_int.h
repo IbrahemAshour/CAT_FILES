13d07d1f94f64519b32e140a66ce30f6dadee75b
Ibrahem Ashour <100946510+IbrahemAshour@users.noreply.github.com>
2024-02-11 07:12:36 +0200

    Drivers of AVR

---
+++ Drivers/HAL/KeyPad/Keypad_int.h
@@ -0,0 +1,14 @@
+/*
+ * Keypad_int.h
+ *
+ *  Created on: 3 Nov 2023
+ *      Author: Alaraby
+ */
+
+#ifndef KEYPAD_INT_H_
+#define KEYPAD_INT_H_
+
+ES_t Keypad_enuInit(void);
+ES_t Keypad_enuGetPressedKey(u8 Copy_u8KPD_PrssedKey);
+
+#endif /* KEYPAD_INT_H_ */
